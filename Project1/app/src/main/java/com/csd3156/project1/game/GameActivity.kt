9ba50f663e60598f69f906a517ded38b64cc2ac0
Cedric <t.yixuancedric@digipen.edu>
2023-02-20 03:13:52 +0800

    Tilt controls implemented
    included grade update for minSDK 29
    Tilt controls true only if phone is horizontal

--- Project1/app/src/main/java/com/csd3156/project1/game/GameActivity.kt
+++ Project1/app/src/main/java/com/csd3156/project1/game/GameActivity.kt
@@ -1,6 +1,10 @@
 package com.csd3156.project1.game
 
 import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -16,11 +20,17 @@ class GameActivity : AppCompatActivity()
 {
     private lateinit var binding: ActivityGameBinding
 
+    private lateinit var sensorManager: SensorManager
+    private var accelerometer: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         WindowCompat.setDecorFitsSystemWindows(window, true)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
@@ -108,5 +118,46 @@ class GameActivity : AppCompatActivity()
                 Snake.direction = "right"
         }
 
+        sensorManager.registerListener(object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                // do nothing
+            }
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                if (event != null && event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                    val x = event.values[0]
+                    val y = event.values[1]
+                    val z = event.values[2]
+                    // perform calculations with x, y, and z values to detect tilt
+
+                    if (x < -2) {
+                        // device is tilted to the right
+                        Snake.alive = true
+                        if (Snake.direction != "left")
+                            Snake.direction = "right"
+                    } else if (x > 2) {
+                        // device is tilted to the left
+                        Snake.alive = true
+                        if (Snake.direction != "right")
+                            Snake.direction = "left"
+                    }
+                    else if (y > 2) {
+                        // device is tilted to the bottom
+                        Snake.alive = true
+                        if(Snake.direction != "up")
+                            Snake.direction = "down"
+                    }
+                    else if (y< 2) {
+                        // device is tilted to the top
+                        Snake.alive = true
+                        if(Snake.direction != "down")
+                            Snake.direction = "up"
+                    }else {
+                        // device is not tilted left or right
+                    }
+
+                }
+            }
+        }, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
     }
 }
